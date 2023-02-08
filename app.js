020dfa95f64e3a5ac8a52712100a561b356e7720
Young <ytm199891@gmail.com>
2023-02-08 17:48:36 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const _ = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
+const items = [3, [2, [3, [4]]]];
 
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log("🗻");
