d49f417a29cff6cb7ff4b37a8466a473b8211b3b
Young <ytm199891@gmail.com>
2023-02-09 17:26:06 +0800

    from callback to promise to async awit. CRAZY.

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const _ = require("lodash");
+const { readFile, read, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const items = [3, [2, [3, [4]]]];
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
 
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("🗻");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `awesome: ${first} ${second}`,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
